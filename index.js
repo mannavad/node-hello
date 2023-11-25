047325c819ae83bfa57393839a0139a1c7143918
mannavad <dhanamannava@gmail.com>
2023-11-25 12:45:38 +0530

    seconmd commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
-  console.log("App started");
+  console.log('app opened')
   res.end(msg);
 });
 
